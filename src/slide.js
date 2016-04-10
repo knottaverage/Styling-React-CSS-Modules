f03b99714521fc4de7ba54f577d0f1139325ed85
knottaverage <steve.knott@iorahealth.com>
2016-04-09 22:22:28 -0400

    adding files

---
+++ src/slide.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import styleable from 'react-styleable'
+
+const { object, string } = React.PropTypes
+
+function Slide(props) {
+  return (
+    <article style={props.style}>
+      <img src={props.image} alt={props.title} />
+      <footer>
+        <h2>{props.title}</h2>
+        <div>{props.children}</div>
+      </footer>
+    </article>
+  )
+}
+
+Slide.propTypes = {
+  image: string.isRequired,
+  style: object,
+  title: string
+}
+
+export default Slide
